910304c2497cd79892904cc9a5e34972ca81346b
sproshchaev <sproshchaev@gmail.com>
2023-03-28 10:18:09 +0500

    init: start task

---
+++ javascript/human-readable-duration-forma/script.js
@@ -0,0 +1,19 @@
+function formatDuration (seconds){
+  let result = true;
+  console.log(result);
+  return result;
+}
+
+// год равен 365 дням, а сутки - 24 часам
+// 0 = now
+// second seconds
+// minute minutes
+// hour hours
+// day days
+// year years
+
+formatDuration(1);
+formatDuration(62);
+formatDuration(120);
+formatDuration(3600);
+formatDuration(3662);
