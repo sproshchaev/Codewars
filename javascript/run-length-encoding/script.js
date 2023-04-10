ccb53e95576b470bd23474b1711f8a4c828080d2
sproshchaev <sproshchaev@gmail.com>
2023-04-10 09:42:26 +0500

    feat: add Run-length encoding

--- javascript/run-length-encoding/script.js
+++ javascript/run-length-encoding/script.js
@@ -1,9 +1,20 @@
-var runLengthEncoding = function(str){
+var runLengthEncoding = function (str) {
   let result = [];
-  
+  let index = 0;
+  let currChar;
+  let countChar = 0;
+  while (index < str.length) {
+    currChar = str[index];
+    while ((currChar == str[index]) && (index < str.length)) {
+      countChar++;
+      index++;
+    }
+    result.push([countChar, currChar]);
+    countChar = 0;
+  }
   return result;
-}
+};
 
-runLengthEncoding(""); // []
-runLengthEncoding("abc"); // [[1,'a'],[1,'b'],[1,'c']]
-runLengthEncoding("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW"); // [[12,'W'],[1,'B'],[12,'W'],[3,'B'],[24,'W'],[1,'B'],[14,'W']]
+console.log(runLengthEncoding("")); // []
+console.log(runLengthEncoding("abc")); // [[1,'a'],[1,'b'],[1,'c']]
+console.log(runLengthEncoding("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW")); // [[12,'W'],[1,'B'],[12,'W'],[3,'B'],[24,'W'],[1,'B'],[14,'W']]
