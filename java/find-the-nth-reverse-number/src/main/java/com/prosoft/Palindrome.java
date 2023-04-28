26844d00bc51ce99a2adee283086d1f5944fafbe
sproshchaev <sproshchaev@gmail.com>
2023-04-28 09:42:33 +0500

    feat: add comment

--- java/find-the-nth-reverse-number/src/main/java/com/prosoft/Palindrome.java
+++ java/find-the-nth-reverse-number/src/main/java/com/prosoft/Palindrome.java
@@ -82,15 +82,26 @@ public class Palindrome {
                         // [1, 3, 1, 1, 3, 1]
                         // [1, 3, 2, 2, 3, 1]
                         // [1, 3, 3, 3, 3, 1] V lengthVar = 6, j = 3,
+                        // ...
+                        // [1, 2, 2, 2, 2, 2, 1] (382)
+                        // [1, 3, 0, 0, 0, 3, 1]
+                        // [1, 3, 1, 0, 1, 3, 1]
+                        // [1, 3, 1, 1, 1, 3, 1]
+                        // [1, 3, 1, 2, 1, 3, 1]
+                        // [1, 3, 2, 0, 2, 3, 1]
+                        // [1, 3, 2, 1, 2, 3, 1]
+                        // [1, 3, 2, 2, 2, 3, 1]
+                        // [1, 3, 2, 3, 2, 3, 1]
+                        // [1, 3, 3, 3, 3, 3, 1] (383) V
 
-                        for (int l = 0; l <= j; l++) {
+                        //for (int l = 0; l <= j; l++) {
 
                             for (int k = 1; k <= lengthVar - 2; k++) {
                                 charArray[k] = (char) (j + '0');
                             }
                             counterReverseNumbers++;
                             System.out.println(counterReverseNumbers + "=" + Arrays.toString(charArray));
-                        }
+                        //}
 
 
                         if (counterReverseNumbers == n) {
