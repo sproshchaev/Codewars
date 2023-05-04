d4dfd33420cb2a6dade40cf7601e806154cb3971
sproshchaev <sproshchaev@gmail.com>
2023-05-04 14:19:17 +0500

    feat: add SolutionTest

--- java/range-extraction/src/test/java/com/prosoft/SolutionTest.java
+++ java/range-extraction/src/test/java/com/prosoft/SolutionTest.java
@@ -11,16 +11,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DisplayName("Methods testing the Solution class")
 class SolutionTest {
 
-    // Solution.rangeExtraction(new int[] {-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20}) # returns "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
-
     @Test
-    @DisplayName("... => ...")
-    void _ExecutedSuccess() {
-        assertEquals(null, Solution.rangeExtraction(null));
+    @DisplayName("rangeExtraction(new int[]{-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20}) => '-10--8,-6,-3-1,3-5,7-11,14,15,17-20'")
+    void rangeExtractionExecutedSuccess() {
+        assertEquals("-10--8,-6,-3-1,3-5,7-11,14,15,17-20", Solution.rangeExtraction(new int[]{-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20}));
     }
 
-    // assertEquals("-6,-3-1,3-5,7-11,14,15,17-20", Solution.rangeExtraction(new int[] {-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20}));
+    @Test
+    @DisplayName("rangeExtraction(new int[]{-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20}) => '-6,-3-1,3-5,7-11,14,15,17-20'")
+    void rangeExtractionExecutedSuccess2() {
+        assertEquals("-6,-3-1,3-5,7-11,14,15,17-20", Solution.rangeExtraction(new int[]{-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20}));
+    }
 
-    // assertEquals("-3--1,2,10,15,16,18-20", Solution.rangeExtraction(new int[] {-3,-2,-1,2,10,15,16,18,19,20}));
+    @Test
+    @DisplayName("rangeExtraction(new int[]{-3, -2, -1, 2, 10, 15, 16, 18, 19, 20}) => '-3--1,2,10,15,16,18-20'")
+    void rangeExtractionExecutedSuccess3() {
+        assertEquals("-3--1,2,10,15,16,18-20", Solution.rangeExtraction(new int[]{-3, -2, -1, 2, 10, 15, 16, 18, 19, 20}));
+    }
 
 }
