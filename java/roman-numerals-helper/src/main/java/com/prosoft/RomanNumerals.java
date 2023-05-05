54b4d7428d550ff45d98fc8f4d06d3db2b73ccd7
sproshchaev <sproshchaev@gmail.com>
2023-05-05 12:16:28 +0500

    feat: updated readme

--- java/roman-numerals-helper/src/main/java/com/prosoft/RomanNumerals.java
+++ java/roman-numerals-helper/src/main/java/com/prosoft/RomanNumerals.java
@@ -3,7 +3,28 @@ package com.prosoft;
 public class RomanNumerals {
 
     public static String toRoman(int n) {
-        return "";
+        StringBuilder result = new StringBuilder();
+        String[] romanNum = new String[]{"I", "IV", "V", "X", "L", "C", "D", "M"};
+        int[] arabicNum = new int[] {1, 4, 5, 10, 50, 100, 500, 1000};
+        int ostatok = n;
+        int curr = -1;
+        while (ostatok > 0) {
+            curr = -1;
+            for (int i = 0; i < arabicNum.length; i++) {
+                if ((ostatok >= arabicNum[i]) && (i < arabicNum.length - 1) && (ostatok < arabicNum[i + 1]))  {
+                    curr = arabicNum[i];
+                    result.append(romanNum[i]);
+                    break;
+                }
+            }
+            if (curr == -1) {
+                curr = arabicNum[arabicNum.length - 1];
+                result.append(romanNum[arabicNum.length -1]);
+            };
+            ostatok = ostatok - curr;
+        }
+        return result.toString();
+
     }
 
     public static int fromRoman(String romanNumeral) {
