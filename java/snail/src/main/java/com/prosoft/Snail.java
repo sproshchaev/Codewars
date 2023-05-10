7b8c22327ddbb8822b0ec3b112fb97ce6975a197
sproshchaev <sproshchaev@gmail.com>
2023-05-10 11:56:22 +0500

    init: start snail

---
+++ java/snail/src/main/java/com/prosoft/Snail.java
@@ -0,0 +1,8 @@
+package com.prosoft;
+
+public class Snail {
+    public static int[] snail(int[][] array) {
+        // enjoy
+        return null;
+    }
+}
---
+++ java/snail/src/test/java/com/prosoft/SnailTest.java
@@ -0,0 +1,17 @@
+package com.prosoft;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Methods testing the Snail class:")
+public class SnailTest {
+
+    @Test
+    @DisplayName("snail({{1,2,3},{4,5,6},{7,8,9}}) ==> {1,2,3,6,9,8,7,4,5}")
+    void toRomanExecutedSuccess() {
+        assertEquals(new int[]{1, 2, 3, 6, 9, 8, 7, 4, 5}, Snail.snail(new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}));
+    }
+
+}
