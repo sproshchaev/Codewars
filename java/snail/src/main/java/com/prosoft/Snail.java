694e1d30fb53c1430e3f5f87eff5de9dfdbdeab7
sproshchaev <sproshchaev@gmail.com>
2023-05-11 12:10:37 +0500

    feat: logic a,b,c,d transferred to methods

--- java/snail/src/main/java/com/prosoft/Snail.java
+++ java/snail/src/main/java/com/prosoft/Snail.java
@@ -6,7 +6,7 @@ public class Snail {
         int[] result = new int[array.length * array[0].length];
 
         // Обход массива
-        //              a
+        //               a
         // array = [[1,2,3,3,3],
         //          [4,5,6,6,6],
         //        d [7,8,9,9,9], b
@@ -17,33 +17,52 @@ public class Snail {
         for (int step = 0; step < 3; step++) {
             System.out.println("'Step: " + (step + 1) + "'");
             // a: 1,2,3,3
-            System.out.print("a=");
-            for (int i = step; i < array[step].length - step; i++) {
-                System.out.print(array[step][i] + " ");
-            }
-            System.out.println("");
+            getA(array, step);
             // b: 3,6,9,4,9
-            System.out.print("b=");
-            for (int i = step + 1; i < array.length - step; i++) {
-                System.out.print(array[i][array[i].length - 1 - step] + " ");
-            }
-            System.out.println("");
+            getB(array, step);
             // c: 9,8,7,6,5
-            System.out.print("c=");
-            for (int i = array[array.length - 1 - step].length - 1 - step - 1; i >= step; i--) {
-                System.out.print(array[array.length - 1 - step][i] + " ");
-            }
-            System.out.println("");
+            getC(array, step);
             // d: 5, 0, 7, 4
-            System.out.print("d=");
-            for (int i = array.length - 1 - step - 1; i >= 1 + step; i--) {
-                System.out.print(array[i][0 + step] + " ");
-            }
-            System.out.println("");
+            getD(array, step);
         }
-
-        
-        // enjoy
         return null;
     }
+
+    private static int[] getA(int[][] array, int step) {
+        // a: 1,2,3,3
+        System.out.print("a=");
+        for (int i = step; i < array[step].length - step; i++) {
+            System.out.print(array[step][i] + " ");
+        }
+        System.out.println("");
+        return null;
+    }
+
+    private static int[] getB(int[][] array, int step) {
+        System.out.print("b=");
+        for (int i = step + 1; i < array.length - step; i++) {
+            System.out.print(array[i][array[i].length - 1 - step] + " ");
+        }
+        System.out.println("");
+        return null;
+    }
+
+    private static int[] getC(int[][] array, int step) {
+        System.out.print("c=");
+        for (int i = array[array.length - 1 - step].length - 1 - step - 1; i >= step; i--) {
+            System.out.print(array[array.length - 1 - step][i] + " ");
+        }
+        System.out.println("");
+        return null;
+    }
+
+    private static int[] getD(int[][] array, int step) {
+        System.out.print("d=");
+        for (int i = array.length - 1 - step - 1; i >= 1 + step; i--) {
+            System.out.print(array[i][0 + step] + " ");
+        }
+        System.out.println("");
+        return null;
+    }
+
 }
