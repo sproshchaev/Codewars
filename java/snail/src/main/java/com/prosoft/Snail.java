bd766aee079ac2d345e2f2fc2ece0ad1e37c57b7
sproshchaev <sproshchaev@gmail.com>
2023-05-11 11:57:03 +0500

    feat: defined a,b,c,d

--- java/snail/src/main/java/com/prosoft/Snail.java
+++ java/snail/src/main/java/com/prosoft/Snail.java
@@ -2,6 +2,47 @@ package com.prosoft;
 
 public class Snail {
     public static int[] snail(int[][] array) {
+
+        int[] result = new int[array.length * array[0].length];
+
+        // Обход массива
+        //              a
+        // array = [[1,2,3,3,3],
+        //          [4,5,6,6,6],
+        //        d [7,8,9,9,9], b
+        //          [0,1,2,3,4],
+        //          [5,6,7,8,9]]
+        //               c
+        // snail(array) #=> [1,2,3,6,9,8,7,4,5]
+        for (int step = 0; step < 3; step++) {
+            System.out.println("'Step: " + (step + 1) + "'");
+            // a: 1,2,3,3
+            System.out.print("a=");
+            for (int i = step; i < array[step].length - step; i++) {
+                System.out.print(array[step][i] + " ");
+            }
+            System.out.println("");
+            // b: 3,6,9,4,9
+            System.out.print("b=");
+            for (int i = step + 1; i < array.length - step; i++) {
+                System.out.print(array[i][array[i].length - 1 - step] + " ");
+            }
+            System.out.println("");
+            // c: 9,8,7,6,5
+            System.out.print("c=");
+            for (int i = array[array.length - 1 - step].length - 1 - step - 1; i >= step; i--) {
+                System.out.print(array[array.length - 1 - step][i] + " ");
+            }
+            System.out.println("");
+            // d: 5, 0, 7, 4
+            System.out.print("d=");
+            for (int i = array.length - 1 - step - 1; i >= 1 + step; i--) {
+                System.out.print(array[i][0 + step] + " ");
+            }
+            System.out.println("");
+        }
+
+        
         // enjoy
         return null;
     }
--- java/snail/src/test/java/com/prosoft/SnailTest.java
+++ java/snail/src/test/java/com/prosoft/SnailTest.java
@@ -11,7 +11,8 @@ public class SnailTest {
     @Test
     @DisplayName("snail({{1,2,3},{4,5,6},{7,8,9}}) ==> {1,2,3,6,9,8,7,4,5}")
     void toRomanExecutedSuccess() {
-        assertEquals(new int[]{1, 2, 3, 6, 9, 8, 7, 4, 5}, Snail.snail(new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}));
+        assertEquals(new int[]{1, 2, 3, 6, 9, 8, 7, 4, 5}, Snail.snail(new int[][] {{1, 2, 3, 3, 3}, {4, 5, 6, 6, 6}, {7, 8, 9, 9, 9},
+                {0, 1, 2, 3, 4}, {5, 6, 7, 8, 9}}));
     }
 
 }
