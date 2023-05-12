5cbc6aef8389a394ee843c5bac529a7e61f50270
sproshchaev <sproshchaev@gmail.com>
2023-05-12 16:24:13 +0500

    feat: Attempt: Execution Timed Out (16000 ms)

--- java/next-smaller-number-with-the-same-digits/src/main/java/com/prosoft/Kata.java
+++ java/next-smaller-number-with-the-same-digits/src/main/java/com/prosoft/Kata.java
@@ -10,40 +10,39 @@ public class Kata {
 
     public static long nextSmaller(long n) {
         char[] charArray = valueOf(n).toCharArray();
-        boolean charArrayHasBeenChanged = false;
-        boolean firstNumberWasFound = false;
-        int indexRight = 0;
-        int indexLeft = 0;
+        boolean charArrayHasBeenChanged = false, firstNumberWasFound = false;
+        int indexRight = 0, indexLeft = 0;
         do {
             charArrayHasBeenChanged = false;
             indexRight = charArray.length - 1;
             indexLeft = charArray.length - 2;
             while ((indexLeft >= 0) && (!firstNumberWasFound)) {
-                if (Character.getNumericValue(charArray[indexLeft]) < Character.getNumericValue(charArray[indexRight])) {
+                if (Character.getNumericValue(charArray[indexLeft]) > Character.getNumericValue(charArray[indexRight])) {
                     swapElements(charArray, indexLeft, indexRight);
                     charArrayHasBeenChanged = true;
                 }
-                if ((charArrayToLong(charArray) > n)) {
+                if (((charArrayToLong(charArray) < n))) {
                     firstNumberWasFound = true;
-                    charArray = findFirstBigger(n, charArray);
+                    charArray = findFirstSmaller(n, charArray);
                 }
                 indexRight--;
                 indexLeft--;
             }
         } while ((charArrayHasBeenChanged) && (!firstNumberWasFound));
-        return charArrayToLong(charArray) == n ? -1 : charArrayToLong(charArray);
+        return (charArrayToLong(charArray) == n) || (charArray.length != valueOf(n).toCharArray().length) ? -1
+                : charArrayToLong(charArray);
     }
 
-    private static char[] findFirstBigger(long n, char[] charArray) {
+    private static char[] findFirstSmaller(long n, char[] charArray) {
         ArrayList<Character> characterArrayList = new ArrayList<>();
-        long firstBigNumber = 0;
-        for (firstBigNumber = n + 1; firstBigNumber <= charArrayToLong(charArray); firstBigNumber++) {
+        long firstBigSmall = 0;
+        for (firstBigSmall = n - 1; firstBigSmall > charArrayToLong(charArray); firstBigSmall--) {
             characterArrayList.clear();
             for (char c : valueOf(n).toCharArray()) {
                 characterArrayList.add(c);
             }
             boolean foundAllDigit = true;
-            for (char c : valueOf(firstBigNumber).toCharArray()) {
+            for (char c : valueOf(firstBigSmall).toCharArray()) {
                 if (!characterArrayList.remove(Character.valueOf(c))) {
                     foundAllDigit = false;
                     break;
@@ -53,13 +52,13 @@ public class Kata {
                 break;
             }
         }
-        return valueOf(firstBigNumber).toCharArray();
+        return valueOf(firstBigSmall).toCharArray();
     }
 
     private static void swapElements(char[] charArray, int indexLeft, int indexRight) {
-        int minVal = Character.getNumericValue(charArray[indexLeft]);
-        charArray[indexLeft] = charArray[indexRight];
-        charArray[indexRight] = Character.forDigit(minVal, 10);
+        int minVal = Character.getNumericValue(charArray[indexRight]);
+        charArray[indexRight] = charArray[indexLeft];
+        charArray[indexLeft] = Character.forDigit(minVal, 10);
     }
 
     private static long charArrayToLong(char[] chArr) {
