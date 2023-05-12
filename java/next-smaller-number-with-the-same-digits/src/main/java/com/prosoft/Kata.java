289c79bdf35cd3a9234b69f5cd24bb8c4c78770c
sproshchaev <sproshchaev@gmail.com>
2023-05-12 12:28:09 +0500

    feat: added tests

--- java/next-smaller-number-with-the-same-digits/src/main/java/com/prosoft/Kata.java
+++ java/next-smaller-number-with-the-same-digits/src/main/java/com/prosoft/Kata.java
@@ -1,9 +1,70 @@
 package com.prosoft;
 
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+import static java.lang.String.valueOf;
+import static java.util.stream.Collectors.joining;
+
 public class Kata {
 
     public static long nextSmaller(long n) {
-        return 0;
+        char[] charArray = valueOf(n).toCharArray();
+        boolean charArrayHasBeenChanged = false;
+        boolean firstNumberWasFound = false;
+        int indexRight = 0;
+        int indexLeft = 0;
+        do {
+            charArrayHasBeenChanged = false;
+            indexRight = charArray.length - 1;
+            indexLeft = charArray.length - 2;
+            while ((indexLeft >= 0) && (!firstNumberWasFound)) {
+                if (Character.getNumericValue(charArray[indexLeft]) < Character.getNumericValue(charArray[indexRight])) {
+                    swapElements(charArray, indexLeft, indexRight);
+                    charArrayHasBeenChanged = true;
+                }
+                if ((charArrayToLong(charArray) > n)) {
+                    firstNumberWasFound = true;
+                    charArray = findFirstBigger(n, charArray);
+                }
+                indexRight--;
+                indexLeft--;
+            }
+        } while ((charArrayHasBeenChanged) && (!firstNumberWasFound));
+        return charArrayToLong(charArray) == n ? -1 : charArrayToLong(charArray);
     }
+
+    private static char[] findFirstBigger(long n, char[] charArray) {
+        ArrayList<Character> characterArrayList = new ArrayList<>();
+        long firstBigNumber = 0;
+        for (firstBigNumber = n + 1; firstBigNumber <= charArrayToLong(charArray); firstBigNumber++) {
+            characterArrayList.clear();
+            for (char c : valueOf(n).toCharArray()) {
+                characterArrayList.add(c);
+            }
+            boolean foundAllDigit = true;
+            for (char c : valueOf(firstBigNumber).toCharArray()) {
+                if (!characterArrayList.remove(Character.valueOf(c))) {
+                    foundAllDigit = false;
+                    break;
+                }
+            }
+            if (foundAllDigit) {
+                break;
+            }
+        }
+        return valueOf(firstBigNumber).toCharArray();
+    }
+
+    private static void swapElements(char[] charArray, int indexLeft, int indexRight) {
+        int minVal = Character.getNumericValue(charArray[indexLeft]);
+        charArray[indexLeft] = charArray[indexRight];
+        charArray[indexRight] = Character.forDigit(minVal, 10);
+    }
+
+    private static long charArrayToLong(char[] chArr) {
+        return Long.parseLong(Stream.of(chArr).map(String::valueOf).collect(joining()));
+    }
+
 
 }
--- java/next-smaller-number-with-the-same-digits/src/test/java/com/prosoft/KataTest.java
+++ java/next-smaller-number-with-the-same-digits/src/test/java/com/prosoft/KataTest.java
@@ -9,9 +9,63 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class KataTest {
 
     @Test
-    @DisplayName(" ==> ")
+    @DisplayName("nextSmaller(21) ==> 12")
     void nextSmallerExecutedSuccess() {
-        assertEquals(0L, Kata.nextSmaller(0L));
+        assertEquals(12, Kata.nextSmaller(21));
+    }
+
+    @Test
+    @DisplayName("nextSmaller(531) ==> 513")
+    void nextSmallerExecutedSuccess2() {
+        assertEquals(513, Kata.nextSmaller(531));
+    }
+
+    @Test
+    @DisplayName("nextSmaller(2071) ==> 2017")
+    void nextSmallerExecutedSuccess3() {
+        assertEquals(2017, Kata.nextSmaller(2071));
+    }
+
+    @Test
+    @DisplayName("nextSmaller(9) ==> -1")
+    void nextSmallerExecutedSuccess4() {
+        assertEquals(-1, Kata.nextSmaller(9));
+    }
+
+    @Test
+    @DisplayName("nextSmaller(111) ==> -1")
+    void nextSmallerExecutedSuccess5() {
+        assertEquals(-1, Kata.nextSmaller(111));
+    }
+
+    @Test
+    @DisplayName("nextSmaller(135) ==> -1")
+    void nextSmallerExecutedSuccess6() {
+        assertEquals(-1, Kata.nextSmaller(135));
+    }
+
+    @Test
+    @DisplayName("nextSmaller(1027) ==> -1")
+    void nextSmallerExecutedSuccess7() {
+        assertEquals(-1, Kata.nextSmaller(1027));
+    }
+
+    @Test
+    @DisplayName("nextSmaller(907) ==> 790")
+    void nextSmallerExecutedSuccess8() {
+        assertEquals(790, Kata.nextSmaller(907));
+    }
+
+    @Test
+    @DisplayName("nextSmaller(441) ==> 414")
+    void nextSmallerExecutedSuccess9() {
+        assertEquals(414, Kata.nextSmaller(441));
+    }
+
+    @Test
+    @DisplayName("nextSmaller(123456798) ==> 123456789")
+    void nextSmallerExecutedSuccess10() {
+        assertEquals(123456789, Kata.nextSmaller(123456798));
     }
 
 }
